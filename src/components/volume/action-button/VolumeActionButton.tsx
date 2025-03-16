84d64afe39e754661fd6de00985e41d904edbdaa
Kamil Kondratowicz <kamil.kondratowicz@owlsdepartment.com>
2025-03-16 17:41:01 +0100

    feat: integrate Dialog component into VolumeActionButton

--- src/components/volume/action-button/VolumeActionButton.tsx
+++ src/components/volume/action-button/VolumeActionButton.tsx
@@ -3,14 +3,23 @@ import './VolumeActionButton.scss';
 import Button from '@/components/button/Button';
 import { VolumeSimple } from '@/types/volume';
 
+import Dialog from '../../dialog/Dialog';
+
 export interface VolumeActionButtonProps {
 	volume: VolumeSimple;
 }
 
-export default function VolumeActionButton(props: VolumeActionButtonProps) {
+export default function VolumeActionButton({ volume }: VolumeActionButtonProps) {
 	return (
-		<Button theme="medium" className="volume-action-button">
-			⚙️
-		</Button>
+		<Dialog
+			hideCloseButton
+			triggerElement={({ open }) => (
+				<Button theme="medium" onClick={open} className="volume-action-button">
+					⚙️
+				</Button>
+			)}>
+			{/* todo: add volume actions */}
+			Actions
+		</Dialog>
 	);
 }
