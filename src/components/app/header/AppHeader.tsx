f2eac6357c1f952b17cebfcb03a8db2355419719
Kamil Kondratowicz <kontakt@kkondratowicz.pl>
2025-05-24 11:57:01 +0200

    feat: add logout button

--- src/components/app/header/AppHeader.tsx
+++ src/components/app/header/AppHeader.tsx
@@ -1,14 +1,23 @@
 import './AppHeader.scss';
 
+import { useAuthStore } from '@/stores/auth';
+
+import Button from '../../button/Button';
 import AppMenu from '../menu/AppMenu';
 import AppSearch from '../search/AppSearch';
 
 export default function AppHeader() {
+	const clearOAuth = useAuthStore(state => state.clearOAuth);
+
 	return (
 		<header className="app-header">
 			<h1 className="app-header__logo">bookshelf</h1>
 
 			<AppMenu />
+
+			<Button theme="medium" className="app-header__logout" onClick={clearOAuth}>
+				⏻<span className="visually-hidden">Logout</span>
+			</Button>
 
 			<div className="app-header__search">
 				<AppSearch />
